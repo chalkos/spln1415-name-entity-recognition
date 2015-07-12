a2eb44a4b1573379f15c66b77a413430da2cafd2
Bruno Ferreira <chalkos@chalkos.net>
2015-07-12 23:21:54 +0100

    pasta htmlreport não vai para o git mas é mantida; MANIFEST tem agora a lista de tudo o que deve constar no ficheiro gerado ao fazer 'make dist'; makefile tem agora mais regras: 'alldoc' gera a documentação html e README.pod, 'README.pod' gera esse ficheiro, 'readmeclean' remove o README.pod, 'htmlreport' gera todos os ficheiros de documentação html, 'htmlreportclean' limpa os ficheiros html gerados, 'htmlreport/all.html' gera um HTML com tudo o que está no README.pod; adicionada mais documentação; no 'review_entities' a parte de merge de coisas que aparecem em minuscula e maiuscula foi comentada; modificada a forma como o Logger funciona, a nova forma está documentada

--- lib/NER/Logger.pm
+++ lib/NER/Logger.pm
@@ -9,7 +9,63 @@ require Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT = qw(TRACE);
 
+our $Active = 0;
+
 # comment the print line to suppress TRACE messages
 sub TRACE{
-  #print STDERR (shift);
+  print STDERR (shift) if( $Active );
 }
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 === NER::Logger ===
+
+NER::Logger - Sub-módulo de output de mensagens de I<debug> usado pelo módulo NER.
+
+=head1 SINOPSE
+
+  use NER::Logger qw(TRACE);
+
+  # enables message output
+  NER::Logger::Active = 1;
+  TRACE("This text is output to STDERR");
+
+  # disables message output
+  NER::Logger::Active = 0;
+  TRACE("This text is not output to STDERR");
+
+=head1 DESCRIÇÃO
+
+Este módulo é apenas uma forma rápida de activar/desactivar mensagens de debug (basta apenas mudar a variável).
+
+=head1 SUBROTINAS
+
+=head2 EXPORT
+
+None by default.
+
+=head2 EXPORT_OK
+
+=head3 TRACE
+
+Recebe uma string (preferencialmente apenas uma linha terminada com C<\n>) como argumento e escreve-a no STDERR caso C<$Active> seja verdadeiro.
+
+=head1 VARIÁVEIS GLOBAIS
+
+B<$Active>: Se for falso, nenhum output é gerado quando se chama a subrotina L<TRACE|/"TRACE">.
+
+=head1 AUTHOR
+
+  B. Ferreira E<lt>chalkos@chalkos.netE<gt>
+  M. Pinto E<lt>mcpinto98@gmail.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2015 by B. Ferreira and M. Pinto
+
+This program is free software; licensed under GPL.
+
+=cut
